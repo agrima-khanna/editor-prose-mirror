62dfe7d324bcff9fe8ca5be4257ac11db3b80dd5
ujjwal170 <84640201+ujjwal170@users.noreply.github.com>
2021-06-28 12:16:01 +0530

    link added

--- src/extensions.js
+++ src/extensions.js
@@ -1,3 +1,7 @@
 //Contains all the extensions used in the editor
+import StarterKit from "@tiptap/starter-kit";
+import Link from '@tiptap/extension-link'
+import {TableT} from './tables'
 
-export var extensions = [];
+
+export var extensions = [StarterKit,Link,TableT];
