3200efaed11a7aa3b7382fdd3f0f1186c7baf65e
ujjwal170 <84640201+ujjwal170@users.noreply.github.com>
2021-06-28 13:38:41 +0530

    changes

--- src/extensions.js
+++ src/extensions.js
@@ -1,7 +1,10 @@
 //Contains all the extensions used in the editor
 import StarterKit from "@tiptap/starter-kit";
 import Link from '@tiptap/extension-link'
-import {TableT} from './tables'
+import Table from '@tiptap/extension-table'
+import TableRow from '@tiptap/extension-table-row'
+import TableCell from '@tiptap/extension-table-cell'
+import TableHeader from '@tiptap/extension-table-header'
 
 
-export var extensions = [StarterKit,Link,TableT];
+export var extensions = [StarterKit,Link,Table,TableRow,TableCell,TableHeader];
